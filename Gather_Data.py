668a5cd1030fa91a8b851bc60914baa958258556
Ethan Qiu HOME <balancein0and1@gmail.com>
2020-07-15 13:05:59 -0400

    Split NYT data into individual days

--- Gather_Data.py
+++ Gather_Data.py
@@ -1,5 +1,11 @@
+import requests
+import json
+import urllib
+from os import getcwd
 import twint
 from geopy.geocoders import Nominatim
+
+
 
 geolocator = Nominatim(user_agent="COVID_Tracker")
 location = geolocator.geocode("Fairfax, Virginia")
@@ -15,7 +21,6 @@ c.Output = "test"
 
 twint.run.Search(c)
 
-import urllib, json, requests
 
 with urllib.request.urlopen("https://geo.fcc.gov/api/census/area?lat=38.8462236&lon=-77.3063733&format=json") as url:
     data = json.loads(url.read().decode())
@@ -27,7 +32,6 @@ print(data['results'][0]['state_fips'])
 print(data['results'][0]['county_fips'])
 print(data['results'][0]['county_name'])
 
-from os import getcwd
 
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-12-2020.csv"
 directory = getcwd()
@@ -35,8 +39,5 @@ print(directory)
 filename = directory + 'somefile.txt'
 r = requests.get(url)
 
-f = open(filename,'wb')
+f = open(filename, 'wb')
 f.write(r.content)
-
-
-
