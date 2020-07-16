43095c65e0b46fe2759ff5718e289daf368c1a8f
Ethan Qiu HOME <balancein0and1@gmail.com>
2020-07-16 10:11:43 -0400

    Patch 1 after catastrophic deletion

--- Splitter.py
+++ Splitter.py
@@ -2,23 +2,37 @@ import csv
 
 
 def write(s, data):
-    with open('Data/Sorted/' + s + ".csv", 'w', newline='') as csvfile:
+    """
+    writes data to file with name s
+    """
+    # console output so i don't get scared
+    print("PRINTING", s)
+    with open('Data/States/' + s + ".csv", 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for row in data:
             writer.writerow(row)
 
 
-with open("Data/us-counties.csv", newline='') as csvfile:
+with open("Data/us-states.csv", newline='') as csvfile:
+    """
+    searches for the data until it reaches the end of the file
+    """
     reader = csv.reader(csvfile, delimiter=',')
-
-    my_list = ["NOT DATA"]
+    my_list = []
+    full_file = []
+    used_list = []
     for row in reader:
-        if row[0] != my_list[-1][0]:
-            print(row[0])
-            write(my_list[-1][0], my_list)
+        full_file.append(row)
+
+    for line in full_file:
+        # finds a new target
+        if not line[1] in used_list:
+            used_list.append(line[1])
+            # searches the entire file for that target
+            for other_line in full_file:
+                if other_line[1] == line[1]:
+                    my_list.append(other_line)
+            # writes and clears list
+            write(line[1], my_list)
             my_list.clear()
-            my_list.append(["NOT DATA"])
-        my_list.append(row)
-
-
