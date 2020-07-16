9acd64195fe4ecda7939a52745658586d9006fec
Ethan Qiu HOME <balancein0and1@gmail.com>
2020-07-16 12:12:19 -0400

    Method development

--- Gather_Data.py
+++ Gather_Data.py
@@ -1,21 +1,25 @@
 import requests
 import json
 import urllib
-from os import getcwd
+import os
 import twint
 from geopy.geocoders import Nominatim
 import datetime
 
 
-def search_twitter(arg, place, date):
+def search_twitter(arg, place, input_date):
     """
     This method searches twitter for the argument
     The location is used as a filter for geotag
     The date is used as the END date, the begin date is ONE DAY before
     """
+    # configuring location
     geolocator = Nominatim(user_agent="COVID_Tracker")
     location = geolocator.geocode(place)
-    print(location.latitude, location.longitude)
+    print("LOCATION FOUND AT", location.latitude, location.longitude)
+
+    # configuring date
+    date = datetime.datetime.strptime(input_date, '%Y-%m-%d')
     # configuring twint
     c = twint.Config()
     c.Lang = "en"
@@ -45,11 +49,40 @@ def reverse_geocode(lat, lon):
     print(data['results'][0]['county_name'])
 
 
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-12-2020.csv"
-directory = getcwd()
-print(directory)
-filename = directory + 'somefile.txt'
-r = requests.get(url)
+def get_github():
+    """
+    getting a raw file from github
+    """
+    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-12-2020.csv"
+    directory = os.getcwd()
+    filename = directory + '\somefile.txt'
+    r = requests.get(url)
+    f = open(filename, 'wb')
+    f.write(r.content)
 
-f = open(filename, 'wb')
-f.write(r.content)
+
+def count_csv():
+    """
+    count the total tweets scraped by twint
+    """
+    line_num = 0
+    with open("Output/tweets.csv", newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            line_num += 1
+
+    return line_num - 1
+
+
+def process(file_name):
+    """
+    The function that does most of the work in this program
+    Takes the file, scrape twitter for trigger words, outputs the results to
+    another compiled csv
+    """
+    search_twitter(arg="j", input_date="2020-07-10", place="Virginia")
+
+
+list = os.listdir("Data/States")
+for file in list:
+    process(file)
