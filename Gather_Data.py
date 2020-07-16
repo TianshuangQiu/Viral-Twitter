c1ddae0c8991289ad3dd886653084fbb884b5651
Ethan Qiu HOME <balancein0and1@gmail.com>
2020-07-16 11:57:05 -0400

    Method development

--- Gather_Data.py
+++ Gather_Data.py
@@ -4,33 +4,45 @@ import urllib
 from os import getcwd
 import twint
 from geopy.geocoders import Nominatim
+import datetime
 
 
+def search_twitter(arg, place, date):
+    """
+    This method searches twitter for the argument
+    The location is used as a filter for geotag
+    The date is used as the END date, the begin date is ONE DAY before
+    """
+    geolocator = Nominatim(user_agent="COVID_Tracker")
+    location = geolocator.geocode(place)
+    print(location.latitude, location.longitude)
+    # configuring twint
+    c = twint.Config()
+    c.Lang = "en"
+    c.Search = arg
+    c.Since = date - datetime.timedelta(days=1)
+    c.Until = date
+    c.Geo = str(location.latitude) + "," + str(location.longitude) + ", 5km"
+    c.Count = True
+    c.Store_csv = True
+    c.Output = "test"
 
-geolocator = Nominatim(user_agent="COVID_Tracker")
-location = geolocator.geocode("Fairfax, Virginia")
-print(location.latitude, location.longitude)
-c = twint.Config()
-c.Lang = "en"
-c.Search = "#coronavirus"
-c.Since = "2020-7-10 0:0:0"
-c.Geo = str(location.latitude) + "," + str(location.longitude) + ", 5km"
-c.Count = True
-c.Store_csv = True
-c.Output = "test"
-
-twint.run.Search(c)
+    twint.run.Search(c)
 
 
-with urllib.request.urlopen("https://geo.fcc.gov/api/census/area?lat=38.8462236&lon=-77.3063733&format=json") as url:
-    data = json.loads(url.read().decode())
-    print(data)
+def reverse_geocode(lat, lon):
+    """
+    Searching the state and county when given latitude and longitude
+    """
+    with urllib.request.urlopen("https://geo.fcc.gov/api/census/area?lat=" + lat + "&lon=" + lon + "&format=json") as url:
+        data = json.loads(url.read().decode())
+        print(data)
 
-print(json.dumps(data, indent=4, sort_keys=True))
+    print(json.dumps(data, indent=4, sort_keys=True))
 
-print(data['results'][0]['state_fips'])
-print(data['results'][0]['county_fips'])
-print(data['results'][0]['county_name'])
+    print(data['results'][0]['state_fips'])
+    print(data['results'][0]['county_fips'])
+    print(data['results'][0]['county_name'])
 
 
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-12-2020.csv"
