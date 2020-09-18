e6aa73a3fc6bc599c93367233500e5e75a26b759
Ethan Qiu HOME <balancein0and1@gmail.com>
2020-09-18 00:10:17 -0400

    backlog push

--- Grapher.py
+++ Grapher.py
@@ -1,10 +1,26 @@
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
-import matplotlib.cbook as cbook
+import matplotlib
 
 import numpy as np
 import pandas as pd
+import csv
 
-msft = pd.read_csv('Output/COMPLETE_AZ.csv')
-fname = cbook.get_sample_data('msft.csv', asfileobj=False)
-with cbook.get_sample_data('msft.csv') as file:
-    msft = pd.read_csv(file)
+data = pd.read_csv('Output/COMPLETE_FL.csv', parse_dates=["Date"])
+data.set_index('Date',inplace=True)
+
+target = ["Cases", "Deaths", "#NoMasks", "#BurnYourMask", "#IWillNotComply",  "#OpenAmerica", "#OpenSchools", "#WearAMask", "#WearADamnMask"]
+
+fig, ax = plt.subplots(nrows=4)
+
+ax[0].bar(data.index, data['#BurnYourMask'], color = "Blue")
+ax[1].bar(data.index, data['#IWillNotComply'], color = "Orange")
+ax[2].bar(data.index, data['#OpenAmerica'], color = "Black")
+ax[3].bar(data.index, data['#OpenSchools'], color = "Green")
+
+ax[0].set_title("#BurnYourMask")
+ax[1].set_title("#IWillNotComply")
+ax[2].set_title("#OpenAmerica")
+ax[3].set_title("#OpenSchools")
+
+plt.show()
